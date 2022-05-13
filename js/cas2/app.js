6b4ab01d01aeaaff752ebfd5409ac46d0147cc88
eldinhasanovic <almirhasanovic40@gmail.com>
2022-05-13 20:58:13 +0200

    commit

---
+++ js/cas2/app.js
@@ -0,0 +1,42 @@
+// unos = prompt("Enter your num1: ");
+
+// for (i = 0; i <= unos; i++) {
+//   console.log(i);
+// }
+// x = prompt("Enter your pocetnu: ");
+// y = prompt("Unesite krajnju: ");
+// for (i = x; i <= y; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// x = Number(prompt("Unesite pocetni broj: "));
+// y = Number(prompt("Unesite krajnji broj: "));
+// sum = 0;
+// for (i = x; i <= y; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// x = Number(prompt("Unesite pocetni broj: "));
+// y = Number(prompt("Unesite krajnji broj: "));
+// for (i = x; i <= y; i++) {
+//   console.log(i ** 2);
+// }
+
+// x = Number(prompt("Unesite broj brojeva: "));
+// sum = 0;
+
+// for (i = 0; i < x; i++) {
+//   j = Number(prompt("Unesite promptove koje hoces"));
+//   sum += j;
+// }
+// console.log(sum);
+
+brzv = Number(prompt("Unesite broj brojeva: "));
+str = " ";
+for (i = 0; i < brzv; i++) {
+  str += "*";
+  console.log(str);
+}
