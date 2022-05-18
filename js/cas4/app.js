5af1b7a2ae44f2e525e63db9c3723be8a96c7b6e
eldinhasanovic <almirhasanovic40@gmail.com>
2022-05-18 20:50:39 +0200

    komite

---
+++ js/cas4/app.js
@@ -0,0 +1,36 @@
+// sifra = "eldin123";
+// unos = prompt("Unesite vrednost x");
+// while (sifra !== unos) {
+//   console.log("vasa sifra nije: ");
+//   console.log(unos);
+//   unos = prompt("pogresna sifra, unesite opet");
+// }
+
+// a = Math.round(Math.random() * 100);
+// x = Number(prompt("Unesite broj 1-10: "));
+// while (x !== a) {
+//   if (x < a) {
+//     x = Number(prompt("Unesite broj 1-100, malo vise: "));
+//   } else {
+//     x = Number(prompt("Unesite broj 1-100, malo nize: "));
+//   }
+// }
+// console.log("Uneli ste tacan broj");
+
+// x = Number(prompt("Unesite broj: "));
+// sum = x;
+// while (x >= 0) {
+//   x = Number(prompt("Unesite broj: "));
+//   sum += x;
+// }
+// console.log(sum);
+// console.log(x);
+
+// x = String(prompt("Unesite neko ime: "));
+// while (x !== "END") {
+//   console.log(x);
+//   x = prompt("Unesite neko ime: ");
+// }
+// console.log("I am done");
+
+x = Number(prompt("Unesite broj brojeva"));
