f92d7986b188f182e802b242d3019b07d62024cc
eldinhasanovic <almirhasanovic40@gmail.com>
2022-05-25 21:00:17 +0200

    komite

--- js/cas5/app.js
+++ js/cas5/app.js
@@ -1,14 +1,12 @@
-var a = prompt("Unesite neku rijec: ");
-var bzsv = a.replace(" ", "").toLowerCase();
-var zadnjeslovo = bzsv.length - 1;
-var jelipalindrom = true;
+// var a = prompt("Unesite neku rijec: ");
+// var bzsv = a.replace(" ", "").toLowerCase();
+// var zadnjeslovo = bzsv.length - 1;
+// var jelipalindrom = true;
 
-for (let i = 0; i < bzsv.length / 2; i++) {
-  if (bzsv[i] !== bzsv[zadnjeslovo - i]) {
-    jelipalindrom = false;
-    break;
-  }
-}
-console.log(jelipalindrom);
-//rip
-// Language: javascript
+// for (let i = 0; i < bzsv.length / 2; i++) {
+//   if (bzsv[i] !== bzsv[zadnjeslovo - i]) {
+//     jelipalindrom = false;
+//     break;
+//   }
+// }
+// console.log(jelipalindrom);
--- js/cas6/app.js
+++ js/cas6/app.js
@@ -1,2 +1,77 @@
-// github copilot test
-a = prompt("Enter the value of a: ");
+// niz = [3, 5, 10, 21, 42, 22, 1];
+// sum = 0;
+// for (i = 0; i < niz.length; i++) {
+//   sum += niz[i];
+// }
+// console.log(sum);
+
+// niz = [3, 5, 10, 21, 42, 22, 1];
+// x = Number(prompt("Unesite broj: "));
+// c = false;
+// for (i = 0; i < niz.length; i++) {
+//   if (niz[i] == x) {
+//     c = true;
+//     break;
+//   }
+// }
+// if (c) {
+//   console.log("Broj je u nizu");
+// } else {
+//   console.log("Broj nije u nizu");
+// }
+
+// niz = [3, 5, 10, 21, 42, 22, 1];
+// najveci = niz[0];
+// najmanji = niz[0];
+// for (i = 0; i < niz.length; i++) {
+//   if (niz[i] > najveci) {
+//     najveci = niz[i];
+//   }
+//   if (niz[i] < najmanji) {
+//     najmanji = niz[i];
+//   }
+// }
+// console.log("najveci je:", najveci);
+// console.log("najmanji je:", najmanji);
+
+// niz = [3, 5, 10, 21, 42, 22, 1, 72, 33, 44, 55, 66, 77, 88, 99, 100];
+// for (i = 0; i < niz.length; i++) {
+//   for (j = i + 1; j < niz.length; j++) {
+//     if (niz[i] > niz[j]) {
+//       c = niz[i];
+//       niz[i] = niz[j];
+//       niz[j] = c;
+//     }
+//   }
+// }
+// console.log("Drugi najveci broj je :", niz[niz.length - 2]);
+
+// niz = [13, 25, 10, 21, 42, 22, 23, 72, 33, 44, 55, 66, 77, 88, 99, 100];
+// najveci = niz[0];
+// najmanji = niz[0];
+// for (i = 0; i < niz.length; i++) {
+//   if (niz[i] > najveci) {
+//     najveci = niz[i];
+//   }
+//   if (niz[i] < najmanji) {
+//     najmanji = niz[i];
+//   }
+// }
+//  console.log("najveci je:", najveci);
+//  console.log("najmanji je:", najmanji);
+// razlika = najveci - najmanji;
+// console.log("razlika je:", razlika);
+
+// niz = [0, 3, 5, 10, 21, 42, 22, 1];
+// c = false;
+// for (i = 0; i < niz.length; i++) {
+//   if (niz[i] == -1 || niz[i] == 0) {
+//     c = true;
+//     break;
+//   }
+// }
+// if (c) {
+//   console.log("Broj je u nizu");
+// } else {
+//   console.log("Broj nije u nizu");
+// }
